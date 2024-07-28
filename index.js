c0d85fa8cc0fad4cfdd045220bb8855f9baf87ec
Wesley Portugal <wesleyportugal250@gmail.com>
2024-07-27 22:44:27 -0300

    aaaaa

---
+++ index.js
@@ -0,0 +1,9 @@
+// Mais Informações - Página de Receita
+
+function mostrarInformacoes () {
+    document.getElementById("maisInformacoesBody").style.display = 'block';
+}
+
+function ocultarInformacoes () {
+    document.getElementById("maisInformacoesBody").style.display = 'none';
+}
