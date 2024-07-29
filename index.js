49ad00cfc471cdb04f967f8077e9d7afd0e31f98
Wesley Portugal <wesleyportugal250@gmail.com>
2024-07-28 23:18:26 -0300

    Progresso

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-// Mais Informações - Página de Receita
+/* Página Inicial - Banner */
 
-function mostrarInformacoes () {
+// Voltar Banner Principal
+function voltarBannerPrincipal() {
+    document.getElementById("bannerPrincipal").style.display = 'none';
+
+    document.getElementById("bannerFeijoada").style.display = 'block';
+}
+// Avançar Banner Principal
+function avancarBannerPrincipal() {
+    document.getElementById("bannerPrincipal").style.display = 'none';
+
+    document.getElementById("bannerFeijoada").style.display = 'block';
+}
+// Voltar Banner Feijoada
+function voltarBannerFeijoada() {
+    document.getElementById("bannerFeijoada").style.display = 'none';
+
+    document.getElementById("bannerPrincipal").style.display = 'block';
+}
+// Avançar Banner Feijoada
+function avancarBannerFeijoada() {
+    document.getElementById("bannerFeijoada").style.display = 'none';
+
+    document.getElementById("bannerPrincipal").style.display = 'block';
+}
+
+
+/* Página de Receita - Mais Informações */
+
+// Mostrar Informações
+function mostrarInformacoes() {
     document.getElementById("maisInformacoesBody").style.display = 'block';
 }
-
-function ocultarInformacoes () {
+// Ocultar Informações
+function ocultarInformacoes() {
     document.getElementById("maisInformacoesBody").style.display = 'none';
 }
+
